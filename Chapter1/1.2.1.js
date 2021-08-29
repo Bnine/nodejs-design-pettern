133c97f2e7274707d0b6624c2285ba7f95572f3d
Bnine <tanreen1@gmail.com>
2021-08-29 17:02:49 +0900

    1.2.1

---
+++ Chapter1/1.2.1.js
@@ -0,0 +1,25 @@
+/*
+if (false) {
+    var x = "hello";
+}
+
+console.log(x);
+*/
+/*
+if (false) {
+    let x = "hello";
+}
+
+console.log(x);
+*/
+/*
+for (let i=0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log("after i => "+i);
+*/
+
+const x = 'This will never change';
+x = 'abcd';
+console.log(x);
